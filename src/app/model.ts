1e0ebb6e95fa6c114aafa097d64ea0d716c7e681
Sylvia Tan <94640886+sylviatan3@users.noreply.github.com>
2023-12-12 15:30:09 -0500

    Add files via upload

---
+++ src/app/model.ts
@@ -0,0 +1,13 @@
+export interface Sessions {
+    id : number;
+    year : number;
+    type : string;
+    title : string;
+    paper : string;
+    topic : string;
+    affiliated_societies : string;
+}
+export interface Topic{
+    id : number;
+    topic: string;
+  }
